e57d0b852e2b406ffb19fb7416e84b2a67941752
phanngotuankiet <ngdacthinh0912@gmail.com>
2025-01-20 17:07:33 +0700

    add env

--- constants.dev.ts
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-    development: {
-      HASURA_ENDPOINT: "http://localhost:11434",
-      CODEGEN_HASURA_GRAPHQL_ADMIN_SECRET: "",
-    },
-    production: {},
-  };
-  
---
+++ constants.ts
@@ -0,0 +1,8 @@
+export const CONFIG = {
+    development: {
+      HASURA_ENDPOINT: "http://localhost:3000/v1/graphql",
+      CODEGEN_HASURA_GRAPHQL_ADMIN_SECRET: "admin",
+    },
+    production: {},
+  };
+  
